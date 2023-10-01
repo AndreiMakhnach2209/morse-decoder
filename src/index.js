30b7410388f1a12c6eceb610944913f909de00b4
Andrei Makhnach <ahdpyxa88@gmail.com>
2023-10-01 17:59:16 +0300

    add solution

--- src/index.js
+++ src/index.js
@@ -35,12 +35,42 @@ const MORSE_TABLE = {
     '---..':  '8',
     '----.':  '9',
     '-----':  '0',
+    ' ': ' '
 };
 
 function decode(expr) {
+    let result = '',
+        symbolMorzeBin = '',
+        symbolMorzeElem = '',
+        symbolMorze = '';
+
     // write your solution here
+    while (expr.length > 0) {
+        symbolMorzeBin = expr.slice(0, 10);
+        expr = expr.slice(10);
+        symbolMorze = '';
+        while (symbolMorzeBin.length > 0) {
+            symbolMorzeElem = symbolMorzeBin.slice(0, 2);
+            symbolMorzeBin = symbolMorzeBin.slice(2);
+            switch (symbolMorzeElem) {
+                case '10':
+                    symbolMorze += '.';
+                    break;
+                
+                case '11':
+                    symbolMorze += '-';            
+                    break;
+                case '**':
+                    symbolMorze = ' ';
+            }
+        }
+        result += MORSE_TABLE[symbolMorze]        
+    }
+    return result;
 }
 
-module.exports = {
-    decode
-}
+console.log(decode("00101010100000000010001011101000101110100000111111**********00001011110000111111000010111000101110100000111010"))
+console.log(decode('000000001100101010100000000010**********00111110110000101011000000101000111011100000111011**********00111010100000101110000011111100001011110000001110**********001010111000001111110011101011**********00101111110000101011000000111100101111100000101010**********0000111111001010101100000000100000101110**********000000001100101010100000000010**********0010111010000000101100111110100011101111**********000011101000001111110000111110'))
+// module.exports = {
+//     decode
+// }
